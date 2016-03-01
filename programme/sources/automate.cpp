8726476385405bf7da54d8178136993012d7df62
Guillaume <guillaume.kheng@insa-lyon.fr>
2016-03-01 11:53:42 +0100

    added automate class files to sources directory

---
+++ programme/sources/automate.cpp
@@ -0,0 +1,83 @@
+﻿/*************************************************************************
+
+						automate  -  description
+						-----------------------
+			début   : 01/03/2016
+		copyright   : (C) 2016 par Team-Papassau - H4101
+
+*************************************************************************/
+​
+//---------- Réalisation de la classe <automate> (fichier automate.h) --
+​
+//---------------------------------------------------------------- INCLUDE
+​
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+​
+//------------------------------------------------------ Include personnel
+#include "automate.h"
+​
+//------------------------------------------------------------- Constantes
+​
+//---------------------------------------------------- Variables de classe
+​
+//----------------------------------------------------------- Types privés
+​
+​
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+​
+//----------------------------------------------------- Méthodes publiques
+// type automate::Méthode ( liste de paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+​
+​
+//------------------------------------------------- Surcharge d'opérateurs
+Automate &Automate::operator = (const Automate &unAutomate)
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+​
+​
+//-------------------------------------------- Constructeurs - destructeur
+Automate::Automate (const Automate &unAutomate)
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de copie de <automate>" << endl;
+#endif
+} //----- Fin de automate (constructeur de copie)
+
+​
+
+Automate::Automate ()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <automate>" << endl;
+#endif
+} //----- Fin de automate
+
+
+Automate::~Automate ()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <automate>" << endl;
+#endif
+} //----- Fin de ~automate
+
+
+  //------------------------------------------------------------------ PRIVE
+
+  //----------------------------------------------------- Méthodes protégées
+
+  //------------------------------------------------------- Méthodes privées
---
+++ programme/sources/automate.h
@@ -0,0 +1,109 @@
+﻿/*************************************************************************
+
+						automate  -  description
+						-----------------------
+			début   : 01/03/2016
+		copyright   : (C) 2016 par Team-Papassau - H4101
+
+*************************************************************************/
+​
+//---------- Interface de la classe <automate> (fichier automate.cpp) ------
+#if ! defined ( AUTOMATE_H )
+#define AUTOMATE_H
+​
+//--------------------------------------------------- Interfaces utilisées
+#include <stack>
+using namespace std;
+​
+//------------------------------------------------------------- Constantes
+​
+//------------------------------------------------------------------ Types
+
+class Symbole;
+class Etat;
+​
+//------------------------------------------------------------------------
+// Rôle de la classe <automate>
+//
+//
+//------------------------------------------------------------------------
+​
+class Automate :
+{
+	//----------------------------------------------------------------- PUBLIC
+
+public:
+	//----------------------------------------------------- Méthodes publiques
+	// type Méthode ( liste de paramètres );
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	// Type de la méthode à confirmer
+	void Lecture();
+	// Mode d'emploi : Méthode initiant l'analyse du programme.
+	//
+	// Contrat :
+	//
+
+
+
+
+	//------------------------------------------------- Surcharge d'opérateurs
+	Automate &operator = (const Automate &unAutomate);
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+
+	//-------------------------------------------- Constructeurs - destructeur
+	Automate (const Automate &unAutomate);
+	// Mode d'emploi (constructeur de copie) :
+	//
+	// Contrat :
+	//
+
+	Automate ();
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	virtual ~Automate ();
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	//------------------------------------------------------------------ PRIVE
+
+protected:
+	//----------------------------------------------------- Méthodes protégées
+
+private:
+	//------------------------------------------------------- Méthodes privées
+
+protected:
+	//----------------------------------------------------- Attributs protégés
+
+	stack<Symbole> pileSymboles;	// Pile des symboles
+	stack<Etat> pileEtats;			// Pile des Etats
+
+
+
+private:
+	//------------------------------------------------------- Attributs privés
+
+	//---------------------------------------------------------- Classes amies
+
+	//-------------------------------------------------------- Classes privées
+
+	//----------------------------------------------------------- Types privés
+
+};
+​
+//----------------------------------------- Types dépendants de <automate>
+​
+#endif // AUTOMATE_H
